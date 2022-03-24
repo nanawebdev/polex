bbc4539654518c70dd29fd63f620228e8a7e2579
nanawebdev <nanaprettysinger@gmail.com>
2022-03-24 13:35:08 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      <HashRouter basename='/polex/'>
+      <HashRouter>
         <App />
       </HashRouter>
     {/* </Provider> */}
